25daff3ba969171f26ea4494c2498aa531a89fb1
Jason McGloin <jason.mcgloin@gmail.com>
2019-11-27 01:02:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add your functions here
+const map = (a, fn) => {
+	return a.map(v => fn(v))
+}
+
+const reduce = (a, fn, i = 0) => {
+	let j = a.find(v  => typeof v != "number") ? true : 0
+	return a.reduce(fn, i || j)
+}

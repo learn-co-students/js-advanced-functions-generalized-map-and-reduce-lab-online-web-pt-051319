516fd2ff002ab2a6206c811ddc47cfc5e891651e
Jess Rezac <jess.rezac@gmail.com>
2019-12-15 17:14:51 -0600

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,3 @@ function reduce(src, func, startingPoint) {
     }
     return value
 }
-
-// expect(reduce(sourceArray, function (e, memo) { return e + memo })).to.equal(6)
-// expect(reduce(sourceArray, function (e, memo) { return e + memo }, startingPoint)).to.equal(106)
-// expect(reduce(sourceArray, function (a, memo) { return !!a && !!memo })).to.be.true
-// expect(reduce(sourceArray, function (a, memo) { return !!a && memo })).to.be.false
-// expect(reduce(sourceArray, function (a, memo) { return a || !!memo })).to.equal(true)
-// expect(reduce(sourceArray, function (a, memo) { return !!a && memo })).to.equal(false)

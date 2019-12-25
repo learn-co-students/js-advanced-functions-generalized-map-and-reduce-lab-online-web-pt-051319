a2995a378fe5892b7066c4690e02d3f30eb65380
nbert100 <nbert14@gmail.com>
2019-12-24 20:59:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(src, func) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(func(src[i]))
+    }
+    return r
+  }
+
+  function reduce(src, func, startingPoint) {
+      let value = startingPoint || src.shift()
+      for (let i = 0; i < src.length; i++) {
+          value = func(src[i], value)
+      }
+      return value
+  }

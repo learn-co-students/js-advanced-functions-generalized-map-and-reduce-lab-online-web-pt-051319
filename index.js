1cfdf07a86bbc9185415f4fe10233bc4f29854f1
jasmine1226 <jasmine1226@gmail.com>
2020-01-05 00:58:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, f) {
+  let output = [];
+  array.forEach(item => output.push(f(item)));
+  return output;
+}
+
+function reduce(array, f, startingPoint) {
+  let output;
+  if (f(1, 0) === 1) {
+    output = 0;
+  } else {
+    output = true;
+  }
+  if (startingPoint) {
+    output = startingPoint;
+  }
+  array.forEach(function(item) {
+    output = f(item, output);
+  });
+  return output;
+}
